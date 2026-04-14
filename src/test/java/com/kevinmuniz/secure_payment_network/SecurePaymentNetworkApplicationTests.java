2371df4b7967861f52926c309ad8028137cfc35a
Kevin Muniz <muniz.kevin@outlook.com>
2026-04-14 19:38:12 -0400

    initial commit

---
+++ src/test/java/com/kevinmuniz/secure_payment_network/SecurePaymentNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinmuniz.secure_payment_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePaymentNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
